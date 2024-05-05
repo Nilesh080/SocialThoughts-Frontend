2d87e6ccf58ca2c6258522d6e1f7d5fceddcf277
Nilesh080 <122158638+Nilesh080@users.noreply.github.com>
2024-05-05 16:07:23 +0530

    added login and registration page

---
+++ src/scenes/homePage/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import Navbar from 'scenes/navbar'
+
+const homePage = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  )
+}
+
+export default homePage
