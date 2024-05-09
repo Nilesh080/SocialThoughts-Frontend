c4d89410115d5fe5b7a6b9c29ce9db23f85f2385
Nilesh080 <122158638+Nilesh080@users.noreply.github.com>
2024-05-09 22:59:39 +0530

    added new deloyed link in api callls

--- src/scenes/widgets/FriendListWidget.jsx
+++ src/scenes/widgets/FriendListWidget.jsx
@@ -14,7 +14,7 @@ const FriendListWidget = ({ userId }) => {
   const getFriends = async () => {
     try {
       const response = await fetch(
-        `http://localhost:3001/users/${userId}/friends`,
+        `https://sociopedia-backend-rouge.vercel.app/users/${userId}/friends`,
         {
           method: "GET",
           headers: { Authorization: `Bearer ${token}` },
